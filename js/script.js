5563dae7600b01869840ef4be0fc292dc444ec42
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-04 15:32:52 +0100

    pushed and printed ten emails

--- js/script.js
+++ js/script.js
@@ -22,6 +22,8 @@ const root = new Vue({
         }
     },
     mounted() {
-        this.getRandomEmails();
+        for (let i = 0; i < 10; i++) {
+            this.getRandomEmails();
+        }
     }
 })
